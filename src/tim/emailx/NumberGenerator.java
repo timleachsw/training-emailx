9ff6fbe71c04fa8d7eac8bfc588a29840e66fd60
Tim Leach <Tim.Leach@softwire.com>
2019-11-20 15:36:13 +0000

    One billion numbers

---
+++ src/tim/emailx/NumberGenerator.java
@@ -0,0 +1,23 @@
+package tim.emailx;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class NumberGenerator {
+    public static void main(String[] args) throws IOException {
+        long nDigits = (long) 1e9;  // a billion!
+
+        try (FileWriter writer = new FileWriter("data/numbers.txt")) {
+            Random random = new Random();
+            try (IntStream intStream = random.ints(nDigits, '0', '9')) {
+                Iterator<Integer> it = intStream.iterator();
+                while (it.hasNext()) {
+                    writer.append((char)it.next().intValue());
+                }
+            }
+        }
+    }
+}
