99afac32773908f905a45982186db7015d2d8a1a
Tim Leach <Tim.Leach@softwire.com>
2019-11-20 11:23:13 +0000

    Created HashMap

--- src/tim/emailx/Main.java
+++ src/tim/emailx/Main.java
@@ -2,6 +2,8 @@ package tim.emailx;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
@@ -11,7 +13,8 @@ public class Main {
 
     public static void main(String[] args) {
         // email matching pattern
-        Pattern emailRegex = Pattern.compile("@softwire\\.com");
+        Pattern emailRegex = Pattern.compile("[\\w.'%+\\-]+@[a-z0-9\\-]+\\.[a-z0-9\\-.]*[a-z0-9\\-]", Pattern.CASE_INSENSITIVE);
+        Pattern domainRegex = Pattern.compile("@(.*)");
 
         File textFile;
         Scanner textScanner;
@@ -25,12 +28,23 @@ public class Main {
             return;
         }
 
-        // count occurrences
+        // find matches
         Stream<MatchResult> matches = textScanner.findAll(emailRegex);
-        long count = matches.count();
 
-        // show count
-        System.out.println(count);
+        // create hashmap
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+        Iterator<MatchResult> it = matches.iterator();
+        for (int i = 0; i < 20; i++) {
+            MatchResult match = it.next();
+            String matchText = match.group();
+
+            // add to hashmap
+            if (hashMap.containsKey(matchText)) {
+                hashMap.put(matchText, hashMap.get(matchText) + 1);
+            } else {
+                hashMap.put(matchText, 1);
+            }
+        }
 
         // close scanner
         textScanner.close();
