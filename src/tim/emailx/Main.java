c2b58253519dc5e8b8241c11512b910c347379a6
Tim Leach <Tim.Leach@softwire.com>
2019-11-20 12:12:09 +0000

    Program prints top 10 domains

--- src/tim/emailx/Main.java
+++ src/tim/emailx/Main.java
@@ -51,9 +51,11 @@ public class Main {
         ArrayList<Map.Entry<String, Integer>> domainCountList = new ArrayList<>(hashMap.entrySet());
         domainCountList.sort(Comparator.comparingInt(Map.Entry::getValue));
 
-        // print all email domains and their counts
-        hashMap.forEach((key, value) -> {
-            System.out.println(key + ": " + value.toString());
-        });
+        // print 10 most common domains in order
+        // have to start from end, because it's sorted lowest first
+        for (int i = 0; i < 10; i++) {
+            Map.Entry<String, Integer> mapEntry = domainCountList.get(domainCountList.size() - i - 1);
+            System.out.println(String.format("%2d. %14s: %3d", i + 1, mapEntry.getKey(), mapEntry.getValue()));
+        }
     }
 }
